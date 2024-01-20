f65d59b1f413c0a95e46be8428bd90cd4d2ce799
Sam Stiles <sam@ironfoxgames.com>
2024-01-20 16:15:48 -0400

    wip

--- client/src/components/board/board.tsx
+++ client/src/components/board/board.tsx
@@ -1,9 +1,38 @@
-import "./board.css";
+/* eslint-disable jsx-a11y/img-redundant-alt */
 
-export default function Board() {
+import "./board.css";
+import boardData from "../../data/board_details.json";
+
+console.log(boardData);
+
+const hexBoardTile = () => {
   return (
-    <div className="Board">
-      <h1>Select A Team To View Progress</h1>
+    <div className={`HexBoardTile ${"green"}`}>
+      <div className="hex-number">123</div>
+      <div className="hex-images">
+        <img
+          src="https://oldschool.runescape.wiki/images/thumb/Coins_detail.png/240px-Coins_detail.png?404bc"
+          alt="hex image"
+          className="hex-image"
+          style={{
+            position: "relative",
+            top: "0px",
+            left: "0px",
+            height: "50px",
+          }}
+        />
+      </div>
+      <div className="hex-title">Any Drop Over 5M</div>
     </div>
   );
+};
+
+export default function Board() {
+  const manyHexes = [];
+
+  for (let i = 0; i < 100; i++) {
+    manyHexes.push(hexBoardTile());
+  }
+
+  return <div className="Board">{manyHexes}</div>;
 }
