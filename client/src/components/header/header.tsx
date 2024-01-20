2027bf669a80c87aa2788a45ad623ff98fe53340
Sam Stiles <sam@ironfoxgames.com>
2024-01-20 15:42:38 -0400

    wip

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -11,30 +11,103 @@ const maximumPoints = undefined;
 function calculateTeamScore(teamDetails: any) {
   return Math.floor(Math.random() * 1000);
 }
-const teamEntry = (teamDetails: any, cssClass: string) => {
-  const teamName = `Team ${teamDetails.leaderName}`;
-  const score = calculateTeamScore(teamDetails);
+
+const getPlacementStringFromPlacementNumber = (placement: number) => {
+  let suffix = "th";
+
+  if (placement % 100 >= 11 && placement % 100 <= 13) {
+    suffix = "th";
+  } else {
+    switch (placement % 10) {
+      case 1:
+        suffix = "st";
+        break;
+      case 2:
+        suffix = "nd";
+        break;
+      case 3:
+        suffix = "rd";
+        break;
+    }
+  }
+
+  return placement + suffix + " place";
+};
+
+const cssClassesForPlacement = {
+  1: "FirstPlace",
+  2: "SecondPlace",
+  3: "ThirdPlace",
+  4: "FourthPlace",
+  5: "FifthPlace",
+};
+
+const getCssClassForPlacement = (placement: number) => {
+  // @ts-ignore
+  return cssClassesForPlacement[placement];
+};
+
+const teamEntry = (
+  teamDetails: any,
+  cssClass: string,
+  score: number,
+  placement: number
+) => {
+  const teamName = `Team ${teamDetails.leaderName}`; /* @ts-ignore */
   return (
-    <div className={`Team animate__animated ${cssClass}`}>
+    <div
+      key={Math.random() * 1000}
+      className={`Team animate__animated ${cssClass}`}
+    >
+      <h2
+        className={`chrome TeamPlacement ${getCssClassForPlacement(placement)}`}
+      >
+        {getPlacementStringFromPlacementNumber(placement)}
+      </h2>
       <h4 className="TeamName">{teamName}</h4>
       <h3 className="TeamScore">
-        {score}
+        {score} points
         {maximumPoints === undefined ? `` : `/ ${maximumPoints}`}
       </h3>
     </div>
   );
 };
 
+const cssClassesByIndex = {
+  0: "animate__fadeInLeft",
+  1: "animate__fadeInTopLeft",
+  2: "animate__fadeInDown",
+  3: "animate__fadeInTopRight",
+  4: "animate__fadeInRight",
+};
+
 const teamEntries = () => {
-  return (
-    <div className="Teams">
-      {teamEntry(babewatchDetails, "animate__fadeInLeft")}
-      {teamEntry(dedionDetails, "animate__fadeInTopLeft")}
-      {teamEntry(kj2Details, "animate__fadeInDown")}
-      {teamEntry(nachoDetails, "animate__fadeInTopRight")}
-      {teamEntry(tengeepeeDetails, "animate__fadeInRight")}
-    </div>
-  );
+  const entries = [];
+
+  const babewatchScore = calculateTeamScore(babewatchDetails);
+  const dedionScore = calculateTeamScore(dedionDetails);
+  const kj2Score = calculateTeamScore(kj2Details);
+  const nachoScore = calculateTeamScore(nachoDetails);
+  const tengeepeeScore = calculateTeamScore(tengeepeeDetails);
+
+  entries.push({ details: babewatchDetails, score: babewatchScore });
+  entries.push({ details: dedionDetails, score: dedionScore });
+  entries.push({ details: kj2Details, score: kj2Score });
+  entries.push({ details: nachoDetails, score: nachoScore });
+  entries.push({ details: tengeepeeDetails, score: tengeepeeScore });
+
+  entries.sort((a, b) => {
+    return b.score - a.score;
+  });
+
+  // @ts-ignore
+  const teamEntries = entries.map((entry, index) => {
+    // @ts-ignore
+    const cssClass = cssClassesByIndex[index];
+    return teamEntry(entry.details, cssClass, entry.score, index + 1);
+  });
+
+  return <div className="Teams">{teamEntries}</div>;
 };
 
 export default function Header() {
